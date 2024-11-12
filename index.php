b975f193ce2ba41b76cf5122367cc6eb4ea18cc9
Finagit <finanurullayali@gmail.com>
2024-11-12 11:40:36 +0700

    melakukan perubahan

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>PERPUSTAKAAN DIGITAL</h1>
+        <form method="GET"action=">
+        <input type="teks"name="search"id=searchBar=placeholder="cari buku....."
+        </form>
+    </header>
+    <section id="daftsr-buku"
+    ></section>
+</body>
+</html>
